7577d390a14b13ae53025ca999dbaed13cbd7656
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-08-16 20:32:19 +0300

    style Footer component

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer>
-            <p>&copy; 2023 City Lovers</p>
+            <p style={{fontWeight: 'bold', fontFamily: 'sans-serif'}} >&copy; 2023 City Lovers</p>
         </footer>
     );
 }
