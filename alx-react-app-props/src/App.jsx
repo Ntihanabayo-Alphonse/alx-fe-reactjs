3c62e1a2cafc37c4743dc382e81129e5dd08f6e5
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-08-16 22:32:09 +0300

    implement useContext hook

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,9 +7,12 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import { UserContext } from './components/UserContext'
+import ProfilePage from './components/ProfilePage'
 
 function App() {
   const [count, setCount] = useState(0)
+  const userData = useState("")
 
   return (
     <>
@@ -39,6 +42,12 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+    {/* User Context */}
+      <UserContext.Provider value={userData}>
+          <ProfilePage />
+      </UserContext.Provider>
+
     </>
   )
 }
