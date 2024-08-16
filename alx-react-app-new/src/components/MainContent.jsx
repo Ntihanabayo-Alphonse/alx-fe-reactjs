4013b15700e14f6fedbf650ad30eb5f633c64a86
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-08-16 20:35:55 +0300

    update styles

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
         <main>
-            <p style={ {color: 'orchid', fontWeight: 'bold',} }>I love to visit <span style={{fontStyle: 'italic', color: '#CF9FFF'}}>New York</span>, <span style={{fontStyle: 'italic', color: '#CF9FFF'}}>Paris</span>, and <span style={{fontStyle: 'italic', color: '#CF9FFF'}}>Tokyo</span>.</p>
+            <p style={ {color: 'orchid', fontWeight: 'bold', fontStyle: 'italic'} }>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
