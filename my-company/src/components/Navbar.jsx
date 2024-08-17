9d65e064ac9c92804f044f312a305774698706fa
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-08-17 14:00:58 +0300

    update using the Link

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
-  return (
-    <div>
-        <nav>
-            <ul>
-                <li><a href="/home">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/services">Services</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-    </div>
-  )
+
+    const linkStyles = {
+        marginRight: '36px'
+    }
+
+    return (
+        <div>
+            <nav>
+                <Link style={linkStyles} to='/home'>Home</Link>
+                <Link style={linkStyles} to='/about'>About</Link>
+                <Link style={linkStyles} to='/services'>Services</Link>
+                <Link to='/contact'>Contact</Link>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
