2cc866f235bce25c8409964ea70b9cb13ace81c2
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-08-17 14:32:06 +0300

    import the component

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,20 +4,24 @@ import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
 import Contact from './components/Contact'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={ <Home />} />
-        <Route path='/home' element={ <Home />} />
-        <Route path='/about' element={ <About />} />
-        <Route path='/services' element={ <Services />} />
-        <Route path='/contact' element={ <Contact />} />
-      </Routes>
-    </BrowserRouter>
+
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
