9208dde47936f1eda2fb61e26d6c2f325e238401
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-08-24 12:30:54 +0300

    add required content

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import create from 'zustand'
 
-const recipeStore = () => {
-  return (
-    <div>recipeStore</div>
-  )
-}
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
 
-export default recipeStore
+export default useRecipeStore;
