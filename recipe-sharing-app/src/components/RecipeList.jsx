f2a6031cf25a10374e42f2951a5edbc486551b6e
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-08-30 11:38:50 +0300

    view list and cancel editRecipeForm if no updates made

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,23 +1,36 @@
 import useRecipeStore from "./recipeStore"
 import DeleteRecipeButton from "./DeleteRecipeButton";
 // import RecipeDetails from "./RecipeDetails";
+import { useState } from "react";
+import EditRecipeForm from "./EditRecipeForm";
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes);
+  const [editingRecipe, setEditingRecipe] = useState(null);
 
-  // console.log(recipes)
+  const handleEdit = (recipe) => {
+    setEditingRecipe(recipe);
+  }
+
+  const handleEditFormClose = () => {
+    setEditingRecipe(null);
+  }
+
   return (
     <div>
-      {recipes.map(recipe => (
-        <>
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-          {/* <DeleteRecipeButton /> */}
-        </>
-      ))}
-
+      <ul>
+        {recipes.map(recipe => (
+            <div key={recipe.id}>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+              <button onClick={() => handleEdit(recipe)}>Edit</button>
+              <DeleteRecipeButton recipeId={recipe.id} />
+            </div>
+        ))}
+        {editingRecipe && (
+          <EditRecipeForm recipe={editingRecipe} onClose={handleEditFormClose}/>
+        )}
+      </ul>
     </div>
   );
 };
