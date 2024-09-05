bc7ef0583ac24dfffe9e51c337e36e4d5ad62c56
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-09-05 10:33:59 +0300

    Set up tailwindcss and postcss

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
+
+// export default {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
