36c89f32b57ab54f2231bbff11c9bdb49a923c2b
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-09-14 14:19:49 +0300

    link card to it's details

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     const [data, setData] = useState([]);
@@ -19,15 +20,18 @@ const HomePage = () => {
 
     return (
         <>
-        <h1 className='font-bold font-sans text-center md:text-4xl text-3xl mb-10'>Recipe Sharing Platform</h1>
+            <h1 className='font-bold font-sans text-center md:text-4xl text-3xl mb-10'>Recipe Sharing Platform</h1>
             <div className="card_container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 md:h-full">
                 {
                     data.map(recipe =>
-                        <div key={recipe.id} className="card rounded-xl overflow-hidden shadow-md h-full sm:h-full hover:shadow-xl duration-500 cursor-pointer">
-                            <img className='w-full h-2/4' src={recipe.image} alt="Recipe Image" />
-                            <h3 className='text-center font-bold text-2xl sm:text-3xl m-5 text-zinc-800 font-sans'>{recipe.title}</h3>
-                            <p className='text-zinc-600 font-sans mx-6'>{recipe.summary}</p>
-                        </div>
+                        <Link to={`/recipe/${recipe.id}`}>
+                            <div key={recipe.id} className="card rounded-xl overflow-hidden shadow-md h-full sm:h-full hover:shadow-xl duration-500 cursor-pointer pb-24">
+                                <img className='w-full h-2/4' src={recipe.image} alt="Recipe Image" />
+                                <h3 className='text-center font-bold text-2xl sm:text-3xl m-5 text-zinc-800 font-sans'>{recipe.title}</h3>
+                                <p className='text-zinc-600 font-sans mx-6'>{recipe.summary}</p>
+                            </div>
+                        </Link>
+
                     )
                 }
             </div>
