692b57146006070118559b59623e1f4d279096a1
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-09-19 21:43:13 +0300

    import and display search

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Search from './components/Search'
 
 function App() {
 
   return (
         <Router>
           <Routes>
-            {/* <Route path='/' element={<Home />} /> */}
+            <Route path='/' element={<Search />} />
           </Routes>
         </Router>
   )
