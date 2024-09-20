2679f692cf23824bd1ff9d339a9285c7c86ac1b3
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-09-20 11:44:30 +0300

    displayed the user details

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,30 +1,57 @@
 import React, { useState, useEffect } from 'react'
+import fetchUserData from '../services/githubService'
 
 const Search = () => {
-
-    const [ search, setSearch ] = useState()
+    const [search, setSearch] = useState()
+    const [userData, setUserData] = useState()
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const handleChange = (e) => {
         const value = e.target.value;
         setSearch(value);
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(search);
+        setLoading(true);
+        setError(null);
+
+        try {
+            const data = await fetchUserData(search);
+            setUserData(data)
+
+        } catch (error) {
+            setError("Looks like we can't find the user");
+        } finally {
+            setLoading(false);
+        }
     }
 
-  return (
-    <form onSubmit={handleSubmit}>
-        <input 
-            type="text"
-            placeholder='Search Github User'
-            name='search-input'
-            onChange={handleChange}
-        />
-        <button className='rounded-md px-6 py-2 text-green-500 bg-blue-950' type="submit">Search</button>
-    </form>
-  )
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder='Search Github Username'
+                    name='search-input'
+                    onChange={handleChange}
+                />
+                <button className='rounded-md px-6 py-2 text-green-500 bg-blue-950' type="submit">Search</button>
+            </form>
+
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+
+            {userData && (
+                <div>
+                    <img src={userData.avatar_url} alt="User Profile" />
+                    <p>{userData.login}</p>
+                    <a href={userData.html_url} target="_blank">View GitHub Profile</a>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default Search
