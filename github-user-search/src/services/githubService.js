a85c65bcf4bd6e5bb0abbf051814ffdc6bf392c4
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-09-21 10:48:23 +0300

    fetch gitHub user details

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
 
-const fetchUserData = async (username) => {
-    const response = await axios.get(`https://api.github.com/users/${username}`)
+const fetchUserData = async ({ search, location, minRepos }) => {
+    const query = `${search}`;
+
+    if(location) {
+        query +=`+location:${location}`;
+    }
+
+    if(minRepos) {
+        query +=`+repos:${minRepos}`;
+    }
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`)
     return response.data;
 }
 
